b59dd43828168c7ca4d917aa672ab6e810e445db
kismetfor <111693609+kismetfor@users.noreply.github.com>
2024-04-07 16:50:39 +0800

    动态规划

--- project/src/MainP34.java
+++ project/src/MainP34.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
+
 public class MainP34 {
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int m = scanner.nextInt();
+
+        int[][] arr = new int[31][31];
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                if (i%2==0 && j%2==0) {
+                    arr[i][j] = 0;
+                } else if (i==1||j==1) {
+                    arr[i][j] = 1;
+                } else {
+                    arr[i][j] = arr[i-1][j] + arr[i][j-1];
+                }
+            }
+        }
+        System.out.println(arr[n][m]);
     }
 }
---
+++ project/src/MainP35.java
@@ -0,0 +1,6 @@
+public class MainP35 {
+    public static void main(String[] args) {
+        //用数组模拟Map 注意0下标 不用0下标
+        
+    }
+}
